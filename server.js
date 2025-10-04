b816d5c2c9dfe2fc87a8e581260b01ec7001fa0a
Sultbaze <zechaxum@gmail.com>
2025-10-04 08:40:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,35 +4,63 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server);
 
-app.get("/", (req, res) => {
-  res.send("SUP backend running 🚀");
+app.use(express.static(__dirname));
+
+io.on("connection", socket => {
+console.log("User connected:", socket.id);
+
+socket.on("chat", msg => {
+io.emit("chat", msg);
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+socket.on("offer", offer => {
+socket.broadcast.emit("offer", offer);
+});
 
-  // Chat relay
-  socket.on("chat", (msg) => {
-    io.emit("chat", msg);
-  });
+socket.on("answer", answer => {
+socket.broadcast.emit("answer", answer);
+});
 
-  // WebRTC signaling
-  socket.on("offer", (offer) => {
-    socket.broadcast.emit("offer", offer);
-  });
-  socket.on("answer", (answer) => {
-    socket.broadcast.emit("answer", answer);
-  });
-  socket.on("candidate", (candidate) => {
-    socket.broadcast.emit("candidate", candidate);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
+socket.on("ice-candidate", cand => {
+socket.broadcast.emit("ice-candidate", cand);
+});
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`SUP server running on ${PORT}`));
+server.listen(PORT, () => console.log("Server running on port " + PORT));
+	
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+io.on("connection", socket => {
+console.log("User connected:", socket.id);
+
+socket.on("chat", msg => {
+io.emit("chat", msg);
+});
+
+socket.on("offer", offer => {
+socket.broadcast.emit("offer", offer);
+});
+
+socket.on("answer", answer => {
+socket.broadcast.emit("answer", answer);
+});
+
+socket.on("ice-candidate", cand => {
+socket.broadcast.emit("ice-candidate", cand);
+});
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Server running on port " + PORT));
+	
